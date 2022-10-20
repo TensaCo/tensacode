1a4521da71e03a6eade5eb9a663c9ccdb93d4c6a
Jacob Valdez <jacobfv123@gmail.com>
2022-10-20 10:46:41 -0500

    meeting sketches

---
+++ tensorcode/operations/encode.py
@@ -0,0 +1,11 @@
+class Encode(Operation):
+
+  OP_NAME: str = "encode"
+
+  def encode(obj, dimensionality, hidden, iteration, ):
+
+    # turn into a graph
+    obj.a = myobjb
+    obj.b = myobjb
+
+    # or turn into a tree
